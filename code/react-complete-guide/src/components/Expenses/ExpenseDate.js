5762ee27edf0b690612a6d2a4b6945ddd2801b46
DH <psycn@naver.com>
2023-06-06 01:56:54 +0900

    2023-06-06

--- code/react-complete-guide/src/components/Expenses/ExpenseDate.js
+++ code/react-complete-guide/src/components/Expenses/ExpenseDate.js
@@ -1,6 +1,6 @@
 import "./ExpenseDate.css";
 
-function ExpneseDate(props) {
+const ExpneseDate = (props) => {
   const year = props.date.getFullYear();
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
@@ -12,6 +12,6 @@ function ExpneseDate(props) {
       <div className="expense-date__day">{day}</div>
     </div>
   );
-}
+};
 
 export default ExpneseDate;
