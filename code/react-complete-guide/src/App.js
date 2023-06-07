32fee5dec8613227676d6aa89130019707b4b108
DH <psycn@naver.com>
2023-06-07 19:42:36 +0900

    2023-06-07 key의 이해, list 다루기, 조건부 다루기

--- code/react-complete-guide/src/App.js
+++ code/react-complete-guide/src/App.js
@@ -1,37 +1,44 @@
+import React, {useState} from "react";
+
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
+
+const DUMMY_EXPENSES = [
+  {
+    id: "eq1",
+    title: "Paper",
+    amount: 94.12,
+    date: new Date(2023, 6, 4),
+  },
+  {
+    id: "eq2",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2023, 6, 4),
+  },
+  {
+    id: "eq3",
+    title: "House Insurance",
+    amount: 22884.67,
+    date: new Date(2023, 6, 4),
+  },
+  {
+    id: "eq4",
+    title: "Person Insurance",
+    amount: 100.07,
+    date: new Date(2023, 6, 4),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "eq1",
-      title: "Paper",
-      amount: 94.12,
-      date: new Date(2023, 6, 4),
-    },
-    {
-      id: "eq2",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2023, 6, 4),
-    },
-    {
-      id: "eq3",
-      title: "House Insurance",
-      amount: 22884.67,
-      date: new Date(2023, 6, 4),
-    },
-    {
-      id: "eq4",
-      title: "Person Insurance",
-      amount: 100.07,
-      date: new Date(2023, 6, 4),
-    },
-  ];
+  const[expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]
+    });
   };
 
   return (
--- code/react-complete-guide/src/components/NewExpense/NewExpense.js
+++ code/react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -1,7 +1,12 @@
+import React, { useState } from "react";
+
+
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditng, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -9,11 +14,21 @@ const NewExpense = (props) => {
     };
 
     props.onAddExpense(expenseData);
+
+    setIsEditing(false);
   };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  }
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  }
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditng && <button onClick={startEditingHandler}>Add New Expense</button>}
+      {isEditng &&<ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}/>}
     </div>
   );
 };
