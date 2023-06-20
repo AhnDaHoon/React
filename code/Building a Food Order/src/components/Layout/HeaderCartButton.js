d4c9812859212139bd781ee0849defb15f5a1620
DESKTOP-BSAFK84\topvel <psycn@naver.com>
2023-06-20 22:09:24 +0900

    장바구니 기능 구현 완료

--- code/Building a Food Order/src/components/Layout/HeaderCartButton.js
+++ code/Building a Food Order/src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
@@ -6,14 +6,32 @@ import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighLighted, setBtnIsHighrighted] = useState(false);
   const cartCtx = useContext(CartContext);
+  const {items} = cartCtx;
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsHighLighted ?  classes.bump : ''}`;
+  useEffect(() => {
+    if(items.length === 0){
+      return;
+    }
+    setBtnIsHighrighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighrighted(false);
+    }, 300)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [items])
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
